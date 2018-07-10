070904fc4e9da2e7414f8c109b583118242de691
akartkam <akartkam@gmail.com>
2018-07-10 23:11:08 +0300

    2 commit

---
+++ grails-app/controllers/qotd/QuoteController.groovy
@@ -0,0 +1,19 @@
+package qotd
+
+class QuoteController {
+
+	static scaffold = true
+	static defaultAction = "home"
+	
+	def quoteService
+	
+	def home() {render "<h1>Real Programmers do not eat Quiche</h1>" }
+	
+	def random () {
+		def randomQuote = quoteService.getRandomQuote()
+		[ quote : randomQuote ]
+	}	
+	
+}
+
+
